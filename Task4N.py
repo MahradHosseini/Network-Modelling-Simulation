37d8d91d84fc8c08b3510fd5be24aae129357aa7
krizantemum <yurekce.altin@metu.edu.tr>
2024-05-10 00:06:08 +0300

    task4 fixed TASK4N

--- Task4N.py
+++ Task4N.py
@@ -11,15 +11,15 @@ wait, the utilisation, and the mean queue length.
 
 
 class MMCSimulation:
-    def __init__(self, lambda_in, mhu_in, c_in, sim_time_in):
+    def __init__(self, lambda_in, mhu_in, c_in, time_limit_in):
         self.lambda_rate = lambda_in  # λ
         self.mhu = mhu_in  # μ
         self.c = c_in  # num of servers
-        self.sim_time = sim_time_in  # how long sim should take
+        self.time_limit = time_limit_in  # how long sim should take
         self.clock = None  # real time
-        self.arrivals = []
         self.services = []
         self.servers = []
+        self.arrivals = []
         self.start = None
         self.rho = None
         self.p_0 = None
@@ -27,40 +27,33 @@ class MMCSimulation:
         self.w_queue = None
         self.w = None
 
-    def generate_arrival_time(self):
-        a = 0
-        while a < self.sim_time:
-            a += random.expovariate(self.lambda_rate)
-            self.arrivals.append(a)
-
     def generate_service_time(self):
         for _ in range(len(self.arrivals)):
             self.services.append(random.expovariate(self.mhu))
 
+    def generate_arrival_time(self):
+        while self.clock < self.time_limit:
+            arrival_time = self.clock + random.expovariate(self.lambda_rate)
+            if arrival_time > self.time_limit:
+                break
+            self.arrivals.append(arrival_time)
+            self.clock = arrival_time
+
     def run_simulation(self):
         self.clock = 0
-        self.generate_arrival_time()
+        self.generate_arrival_time()  # generate arrivals
         self.generate_service_time()
-        while self.clock < self.sim_time:
-            self.clock = self.arrivals[0]
-            if len(self.servers) >= self.c:
-                self.start = min(self.servers)
-                self.servers.pop(0)
+        while self.clock < self.time_limit and len(self.arrivals) !=0:
+            if len(self.servers) == self.c:
+                self.clock = min(self.servers)
+                self.servers.remove(min(self.servers))
             else:
-                self.start = self.arrivals[0]
+                self.clock += self.arrivals[0]
             service_time = self.services[0]
-            departure_time = self.start + service_time
-            self.services.pop(0)
+            departure_time = self.clock + service_time
             self.servers.append(departure_time)
-            self.arrivals.pop(0)
-            if len(self.arrivals) == 0:
-                break
+            self.services.remove(service_time)
 
-            while len(self.servers) > 0:
-                if min(self.servers) < self.arrivals[0]:
-                    self.servers.remove(min(self.servers))
-                else:
-                    break
         self.calculations()
 
     def calculations(self):
@@ -89,13 +82,16 @@ class MMCSimulation:
 
 lambda_rate = 0.8
 mhu = 1
-c = 1
+c = 2
 sim_time = 1000
 
 simulation = MMCSimulation(lambda_rate, mhu, c, sim_time)
 simulation.run_simulation()
-simulation.calculations()
 print("Utilization (ρ)", simulation.rho)
 print("Mean queue length", simulation.l_queue)
 print("Average Response Time in the System (W):", simulation.w)
 print("Average waiting time for queue:", simulation.w_queue)
+
+
+
+
